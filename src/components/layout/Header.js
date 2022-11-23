f892814e4637d9793d786a2e0715931647066bad
Ronaldo Mine <andremine98@gmail.com>
2022-11-23 22:23:50 +0100

    Update Header

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,9 +1,8 @@
-import {useEffect, useState} from "react";
-import {Breadcrumb, Button, Col, Drawer, Dropdown, Row,} from "antd";
+import {useEffect} from "react";
+import {Breadcrumb, Button, Col, Dropdown, Row,} from "antd";
 import {Link, NavLink} from "react-router-dom";
 import {useAuth} from "../../hooks/useAuth";
-import Title from "antd/lib/typography/Title";
-import {profile_svg, setting_svg, toggler_svg} from "../../icons/svg";
+import {profile_svg, toggler_svg} from "../../icons/svg";
 
 const menu = (signOut) => [{
     label: <Link to={"#"}>My Profile</Link>, key: 0
@@ -17,9 +16,6 @@ const menu = (signOut) => [{
 function Header({name, onPress}) {
     const {user: auth, signOut} = useAuth();
     useEffect(() => window.scrollTo(0, 0));
-    const [visible, setVisible] = useState(false);
-    const showDrawer = () => setVisible(true);
-    const hideDrawer = () => setVisible(false);
 
     return (<>
         <Row gutter={[24, 0]}>
@@ -46,29 +42,6 @@ function Header({name, onPress}) {
                 >
                     {toggler_svg}
                 </Button>
-                <Button type="link" onClick={showDrawer}>
-                    {setting_svg}
-                </Button>
-                <Drawer
-                    className="settings-drawer"
-                    mask={true}
-                    width={360}
-                    onClose={hideDrawer}
-                    placement={"right"}
-                    open={visible}
-                >
-                    <div layout="vertical">
-                        <div className="header-top">
-                            <Title level={4}>
-                                Configurator
-                            </Title>
-                        </div>
-                        <div className="sidebar-color">
-                            <Title level={5}>Sidebar Color</Title>
-
-                        </div>
-                    </div>
-                </Drawer>
                 <Dropdown menu={{items: menu(signOut)}} trigger={["click"]} className="header-notifications-dropdown">
                     <Link to="/sign-in" className="ant-dropdown-link">
                         {profile_svg}
